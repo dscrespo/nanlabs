e57a6d95e124bc7d22367f1bd24b77718f82e4c6
Diego Crespo <diego.crespo@grupoesfera.com.ar>
2022-11-29 15:51:25 -0300

    obtengo la configuracion por properties

--- src/main/java/com/nanlabs/trellomanagement/client/TrelloClient.java
+++ src/main/java/com/nanlabs/trellomanagement/client/TrelloClient.java
@@ -4,6 +4,7 @@ import com.nanlabs.trellomanagement.model.TO.CardTO;
 import com.nanlabs.trellomanagement.model.card.CardList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,8 @@ public interface TrelloClient {
     @PostMapping(path = "/card/")
     void insertCard(CardTO card);
 
-    @GetMapping(path = "/boards/63852f86684b88066b97d8fd/lists")
-    List<CardList> getIdsBoards(@RequestParam String key, @RequestParam String token);
+    @GetMapping(path = "/boards/{boardId}/lists")
+    List<CardList> getIdsBoards(final @PathVariable String boardId,
+                                final @RequestParam String key,
+                                final@RequestParam String token);
 }
--- src/main/java/com/nanlabs/trellomanagement/service/CardService.java
+++ src/main/java/com/nanlabs/trellomanagement/service/CardService.java
@@ -4,12 +4,22 @@ import com.nanlabs.trellomanagement.client.TrelloClient;
 import com.nanlabs.trellomanagement.model.TO.CardTO;
 import com.nanlabs.trellomanagement.model.card.CardList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CardService<T> {
+
+    @Value(("${trello.api.key.public}"))
+    private String key;
+
+    @Value(("${trello.api.key.token}"))
+    private String token;
+
+    @Value(("${trello.board.id}"))
+    private String boardId;
 
     @Autowired
     CardFactory cardFactory;
@@ -24,7 +34,7 @@ public class CardService<T> {
     }
 
     public void getBoardsIds(){
-        List<CardList> idList = client.getIdsBoards("4f8082379869adc2345e251249b5809d", "dcd0cc8704ab7e35a308ed9617d461145d1edf79a45b3d4392f22f80670ff241");
+        List<CardList> idList = client.getIdsBoards(boardId, key, token);
         System.out.println(idList);
     }
 }
